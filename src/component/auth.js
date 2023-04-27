17af66a94a20e0ea5338f4ae3bebc1aaa54c5e08
LeeHyeoisung <LeeHyeoiSung>
2023-04-27 20:57:40 +0900

    as

--- src/component/auth.js
+++ src/component/auth.js
@@ -3,14 +3,12 @@ import React from "react";
 const Users = 
     { ID: "ddd@ddd", Password: "123", name: "Kim" }
 ;
-
 export function Auth() {
     const User = Users.find(
           (User) => User.ID === (온값) && User.Password === (온값)
 
         )
         //if 조건 걸고 실패시 알림창 띄우기
-        
         return User;
     };
 
